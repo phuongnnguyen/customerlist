14cfe4813a0f88e71f7d9223b99fafa2e609d013
phuongnguyen <phuongnguyen952501@gmail.com>
2019-06-20 17:38:17 +0700

    Fix

--- index.js
+++ index.js
@@ -15,17 +15,6 @@ mongoose.promise = global.Promise;
 // config
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
-
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Request-Method', '*');
-    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PATCH, DELETE, PUT');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 
 // routes
 app.use('/', require('./routes/customers'));
